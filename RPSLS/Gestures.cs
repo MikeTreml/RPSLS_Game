42c376a3e257757084ae0fe1bfe620013dd70c5f
unknown <miketreml@mac.com>
2020-09-18 13:00:43 -0500

    Created player class with two sub classes. Sub class human and Sub class AI. Each class will Inherit the string name, int choice and int wins. Created a class for getstures. this will contain a list of gestures in the string format. Since they are in the list the postion point will work as the move selection.

---
+++ RPSLS/Gestures.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+
+    class Gestures
+    {
+        //member Variables 
+        public List<string> gesters;
+        //Constructor
+        public Gestures()
+        {
+            gesters = new List<string>();
+            gesters.Add("Rock");
+            gesters.Add("Paper");
+            gesters.Add("Scissors");
+            gesters.Add("Lizzard");
+            gesters.Add("Spock");
+
+        }
+        //member methods
+    }
+}
